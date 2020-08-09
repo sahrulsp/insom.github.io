4b3e19548c9cbce143192d44aa18b1e787936bde
sahrulsp <sahrulsidikpermana@gmail.com>
2020-08-09 19:27:09 +0700

    initial commit

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
+    { author: "antjedtta_", quotes: "Semoga Hari-harimu Menyenangkan:)\nBagaimana Kuliahnya?" },
+    { author: "standinshd", quotes: "Gimana Sekarang Udah Mulai Suka Drakor?" },
+    { author: "standinshd", quotes: "i love you but i'm letting go." },
+    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday hehe." },
+    { author: "standinshd", quotes: "Cause you know what they say\nIf you love somebody\nGotta set them free." },
+    { author: "standinshd", quotes: "I'm sorry for loving you." },
+];
---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Mbak", "PutihHartini", "My Crush", "PutihHartini", ""],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
